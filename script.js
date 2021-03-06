2229a54d4a633fe81a0324c61a8341710aa2607f
sjugurdzija <jugurdzija.sasa@gmail.com>
2021-03-06 22:10:12 +0100

    Add design changes.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
+document.addEventListener("DOMContentLoaded", function(){
+  for(var i=1; i<=100; i++){
+      var select = document.getElementById("years");
+      var option = document.createElement("OPTION");
+      select.options.add(option);
+      option.text = i;
+      option.value = i;
+  }
+});
+
+function compute(){
     p = document.getElementById("principal").value;
-    
+}
+
+function displaySliderValue() {
+   var val = document.getElementById("rate").value
+   document.getElementById('output').innerHTML = val
 }
         
